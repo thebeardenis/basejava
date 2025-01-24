2bbe959c95484ad5a412323fbb10f17568e6d0ab
thebear <denis_2000_kolesnik@mail.ru>
2025-01-24 16:18:16 +0300

    Remake details

--- src/com/topjava/webapp/storage/ArrayStorage.java
+++ src/com/topjava/webapp/storage/ArrayStorage.java
@@ -49,9 +49,7 @@ public class ArrayStorage {
     }
 
     public void clear() {
-        for (int i = 0; i < size; i++) {
-            storage[i] = null;
-        }
+        Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
@@ -69,7 +67,7 @@ public class ArrayStorage {
 
     public Resume[] getAll() {
 
-        return Arrays.copyOf(storage, size);
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     public int size() {
