3c3327c38d38c428a4484869b0ff9186e7d4f382
thebear <denis_2000_kolesnik@mail.ru>
2025-01-26 10:45:33 +0300

    HW3 finish, abstract class and pattern.

--- src/com/topjava/webapp/storage/ArrayStorage.java
+++ src/com/topjava/webapp/storage/ArrayStorage.java
@@ -2,12 +2,16 @@ package com.topjava.webapp.storage;
 
 import com.topjava.webapp.model.Resume;
 
-import java.util.Arrays;
+public class ArrayStorage extends AbstractArrayStorage {
 
-public class ArrayStorage extends AbstractArrayStorage{
-    private static final int ARRAY_LENGTH = 100000;
-    private final Resume[] storage = new Resume[ARRAY_LENGTH];
-    private int size;
+    @Override
+    protected void fillDeleteElement(int index) {
+        storage[index] = storage[size - 1];
+    }
+    @Override
+    protected void insertElement(Resume r, int index) {
+        storage[index] = r;
+    }
 
     protected int getIndex(String uuid) {
         for (int index = 0; index < size; index++) {
@@ -16,48 +20,6 @@ public class ArrayStorage extends AbstractArrayStorage{
             }
         }
         return -1;
-    }
-
-    public void update(Resume r) {
-        int index = getIndex(r.getUuid());
-        if (index != -1) {
-            storage[index] = r;
-        } else {
-            System.out.println("Don't have " + r.getUuid() + " in storage.");
-        }
-    }
-
-    public void delete(String uuid) {
-        int index = getIndex(uuid);
-        if (index != -1) {
-            storage[index] = storage[size-1];
-            storage[size-1] = null;
-            size--;
-        } else {
-            System.out.println("Don't have " + uuid + " in storage.");
-        }
-    }
-
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
-    }
-
-    public void save(Resume r) {
-        if (getIndex(r.getUuid()) == -1) {
-            if (size < ARRAY_LENGTH) {
-                storage[size++] = r;
-            }else {
-                System.out.println("Storage is overflow.");
-            }
-        } else {
-            System.out.println("Resume "+r.getUuid()+" already in storage.");
-        }
-    }
-
-    public Resume[] getAll() {
-
-        return Arrays.copyOfRange(storage, 0, size);
     }
 
 }
